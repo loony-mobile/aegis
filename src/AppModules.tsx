b5a79a365b92724461fa805fc13739b6321df171
Sankar Boro <sankar.boro@yahoo.com>
2025-01-19 10:11:22 +0530

    Go back

--- src/AppModules.tsx
+++ src/AppModules.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, BackHandler} from 'react-native';
 import AegisModule from './appmodules/AegisModule.tsx';
 import {useTheme} from './context/AppProvider';
 import {STYLES, styles} from './styles';
@@ -14,23 +14,45 @@ import PhoneModule from './appmodules/PhoneModules.tsx';
 import MessagesModule from './appmodules/MessagesModule.tsx';
 import ContactsModule from './appmodules/ContactsModule.tsx';
 
+const MODULES = {
+  HOME: 'HOME',
+  AEGIS: 'AEGIS',
+  PHONE: 'PHONE',
+  MESSAGES: 'MESSAGES',
+  CONTACTS: 'CONTACTS',
+};
+
 function AppModules(): React.JSX.Element {
   const appTheme = useTheme();
   const theme: Indexer = STYLES[appTheme];
 
-  const [module, setModule] = useState('HOME');
+  const [module, setModule] = useState(MODULES.HOME);
 
-  if (module === 'AEGIS') {
+  useEffect(() => {
+    const backAction = () => {
+      setModule(MODULES.HOME);
+      return true;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      backAction,
+    );
+
+    return () => backHandler.remove();
+  }, []);
+
+  if (module === MODULES.AEGIS) {
     return <AegisModule />;
   }
 
-  if (module === 'PHONE') {
+  if (module === MODULES.PHONE) {
     return <PhoneModule />;
   }
-  if (module === 'MESSAGES') {
+  if (module === MODULES.MESSAGES) {
     return <MessagesModule />;
   }
-  if (module === 'CONTACTS') {
+  if (module === MODULES.CONTACTS) {
     return <ContactsModule />;
   }
 
@@ -38,19 +60,19 @@ function AppModules(): React.JSX.Element {
     <View style={[theme.con, styles.container]}>
       <SearchBarWithClear />
       <Row>
-        <CardComponent onPress={() => setModule('AEGIS')}>
+        <CardComponent onPress={() => setModule(MODULES.AEGIS)}>
           <SimpleLineIcons name="lock" size={30} color="white" />
           <TextComponent>Aegis</TextComponent>
         </CardComponent>
-        <CardComponent onPress={() => setModule('PHONE')}>
+        <CardComponent onPress={() => setModule(MODULES.PHONE)}>
           <SimpleLineIcons name="phone" size={30} color="white" />
           <TextComponent>Phone</TextComponent>
         </CardComponent>
-        <CardComponent onPress={() => setModule('CONTACTS')}>
+        <CardComponent onPress={() => setModule(MODULES.CONTACTS)}>
           <AntDesign name="contacts" size={30} color="white" />
           <TextComponent>Contacts</TextComponent>
         </CardComponent>
-        <CardComponent onPress={() => setModule('MESSAGES')}>
+        <CardComponent onPress={() => setModule(MODULES.MESSAGES)}>
           <AntDesign name="message1" size={30} color="white" />
           <TextComponent>Messages</TextComponent>
         </CardComponent>
